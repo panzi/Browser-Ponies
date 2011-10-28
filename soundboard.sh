97d721acbc5d1136b7d5b74c719798297874330b
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2011-10-28 21:27:19 +0200

    updated resources to Desktop Ponies 1.35.2

--- soundboard.sh
+++ soundboard.sh
@@ -2,6 +2,13 @@
 
 for pony in sounds/*; do
 	echo "processing $pony..."
+	for mp3 in "$pony"/*.mp3; do
+		ogg="$pony/`basename "$mp3" .mp3`.ogg"
+		if [ ! -e "$ogg" ]; then
+			echo "converting $mp3 to ogg..."
+			ffmpeg -i "$mp3" "$ogg" > /dev/null || exit 1
+		fi
+	done
 	i=1
 	for mp3 in "$pony"/*.mp3; do
 		text=`basename "$mp3" .mp3`
