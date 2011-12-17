ca7ad37b75ccba8dd18b86c99650804c699ca7a7
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2011-12-17 04:39:47 +0100

    cookie code fix

--- everywhere.js
+++ everywhere.js
@@ -77,7 +77,8 @@ function updateConfig () {
 	expires.setTime(expires.getTime() + (100*365*24*60*60*1000));
 	var suffix = '; expires='+expires.toGMTString()+'; path='+window.location.pathname;
 	for (var name in cookies) {
-		document.cookie = 'ponies.'+cookies[name]+suffix;
+		document.cookie = encodeURIComponent('ponies.'+name)+'='+
+			encodeURIComponent(cookies[name])+suffix;
 	}
 }
 
